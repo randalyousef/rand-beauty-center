d24627093f5c783273e5a40cabbc0de517b35b7e
randalyousef <81861076+randalyousef@users.noreply.github.com>
2021-04-03 14:35:24 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  // choses-slider
+
+  // AOS.init();
+
+
+  // $(window).scroll(() => {
+  //   if ($(this).scrollTop() > 20) {
+  //     $(".navbar").addClass("fixed-navbar");
+  //     $("#floating").css("opacity", "0.8");
+  //   } else {
+  //     $(".navbar").removeClass("fixed-navbar");
+  //     $("#floating").css("opacity", "0");
+  //   }
+  // });
+
+  // $("#floating").click(() => {
+  //   $("html, body").animate(
+  //     {
+  //       scrollTop: 0,
+  //     },
+  //     50
+  //   );
+  // });
+
+});
